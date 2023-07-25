01e91b2ae474ae168c562de6ee1464b233573e49
Waaatanuki <watanukipublic@gmail.com>
2023-07-25 18:51:53 +0800

    feat: 优化storage使用

--- src/script/background.ts
+++ src/script/background.ts
@@ -1,35 +1,47 @@
-const resultUrlREG = /granbluefantasy.jp\/#result_multi\/[0-9]+/
+/* eslint-disable no-console */
+import { load } from 'cheerio'
+import { storageDemo } from '@/logic/storage'
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.url && tab.favIconUrl && resultUrlREG.test(changeInfo.url)) {
-    chrome.tabs.sendMessage(tabId, { todo: 'getBattleResult' }).then((res) => {
-      if (res) {
-        chrome.notifications.create({
-          iconUrl: `/assets/${res.name}.png`,
-          message: ' Get☆Daze!',
-          type: 'basic',
-          title: '通知',
-        })
-      }
+(() => {
+  console.log(storageDemo)
+  console.log(1233123333)
+
+  const $ = load('<h2 class="title">Hello world</h2>')
+
+  console.log($('h2.title').text())
+
+  const resultUrlREG = /granbluefantasy.jp\/#result_multi\/[0-9]+/
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url && tab.favIconUrl && resultUrlREG.test(changeInfo.url)) {
+      chrome.tabs.sendMessage(tabId, { todo: 'getBattleResult' }).then((res) => {
+        if (res) {
+          chrome.notifications.create({
+            iconUrl: `/assets/${res.name}.png`,
+            message: ' Get☆Daze!',
+            type: 'basic',
+            title: '通知',
+          })
+        }
+      })
+    }
+  })
+
+  const setBadge = function () {
+    chrome.storage.local.get(null, (items) => {
+      const total = Object.hasOwnProperty.call(items, 'QuestTableData')
+        ? Object.keys(items).length - 1
+        : Object.keys(items).length
+      const color = total <= 100 ? '#25b506' : total <= 200 ? '#becc00' : '#be0000'
+      chrome.action.setBadgeText({ text: total.toString() }, () => {
+        chrome.action.setBadgeBackgroundColor({ color })
+      })
     })
   }
-})
 
-const setBadge = function () {
-  chrome.storage.local.get(null, (items) => {
-    const total = Object.hasOwnProperty.call(items, 'QuestTableData')
-      ? Object.keys(items).length - 1
-      : Object.keys(items).length
-    const color = total <= 100 ? '#25b506' : total <= 200 ? '#becc00' : '#be0000'
-    chrome.action.setBadgeText({ text: total.toString() }, () => {
-      chrome.action.setBadgeBackgroundColor({ color })
-    })
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.remove('materialInfo')
+    setBadge()
   })
-}
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.remove('materialInfo')
-  setBadge()
-})
-
-chrome.storage.onChanged.addListener(() => setBadge())
+  chrome.storage.onChanged.addListener(() => setBadge())
+})()
